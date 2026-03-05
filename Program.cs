e4d9006efc4402139857c84ed4fc87de9862eacd
Moises <moiseslopez1155@gmail.com>
2026-03-04 19:54:04 -0600

    Ingreso y muestra de datos.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Dictionary<string, double> estudiantes = new Dictionary<string, double>();
+
+estudiantes.Add("Moisés", 89);
+estudiantes.Add("Carlos", 70);
+estudiantes.Add("Marcos", 61);
+
+foreach(var item in estudiantes)
+{
+    Console.WriteLine($"Nombre: {item.Key}, Nota: {item.Value}. ");
+}
